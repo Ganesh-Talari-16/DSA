89b785c079744c361e13ba9c45cce8ccea896b87
GaneshTalari <ganeshpnr16@gmail.com>
2026-06-21 11:49:14 +0530

    Time: 4 ms (40.81%) | Memory: 12.5 MB (91.82%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,14 +1,30 @@
-class Solution(object):
+class Solution:
     def nextGreaterElement(self, nums1, nums2):
-        res = []
-        for ele in nums1:
-            nxtge = -1
-            for i in range(len(nums2)):
-                
-                if ele == nums2[i]: 
-                    for j in range(i+1,len(nums2))  :
-                        if nums2[j] > ele:
-                            nxtge = nums2[j]
-                            break
-            res.append(nxtge)
+        # nums1idx = {n:i for i,n in enumerate(nums1)}
+        # res = [-1] * len(nums1)
+
+        # for i in range(len(nums2)):
+        #     if nums2[i] not in nums1idx:
+        #         continue
+        #     for j in range(i+1,len(nums2)):
+        #         if nums2[j] > nums2[i]:
+        #             idx = nums1idx[nums2[i]]
+        #             res[idx] = nums2[j]
+        #             break
+        # return res
+        nums1idx = {n:i for i,n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+        stack = []
+
+        for i in range(len(nums2)):
+
+            while stack and nums2[i] > stack[-1]:
+                val = stack.pop()
+
+                if val in nums1idx:
+                    idx = nums1idx[val]
+                    res[idx] = nums2[i]
+
+            stack.append(nums2[i])
+
         return res
