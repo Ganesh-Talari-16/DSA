56292f7b3d09c1b6d46b3a102c7ae8d9fed19343
GaneshTalari <ganeshpnr16@gmail.com>
2026-07-14 10:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -15,10 +15,5 @@ class Solution:
                 current.next = list2
                 list2 = list2.next
             current = current.next
-        # Attach remaining list if exist
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
-        #current.next = list1 or list2
+        current.next = list1 or list2
         return dummy.next
